c19fcfbcf466d627fc26236622f400c83c1be98e
Marvin Arnold <marvin@geeky.rocks>
2019-01-12 13:33:38 -0600

    Initial section definitions and gatsby skeleton

---
+++ src/pages/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import Banner from '../components/Banner'
+
+import pic01 from '../assets/images/pic01.jpg'
+import pic02 from '../assets/images/pic02.jpg'
+import pic03 from '../assets/images/pic03.jpg'
+import pic04 from '../assets/images/pic04.jpg'
+import pic05 from '../assets/images/pic05.jpg'
+import pic06 from '../assets/images/pic06.jpg'
+
+class HomeIndex extends React.Component {
+    render() {
+
+        return (
+            <Layout>
+                <Helmet
+                    title="Myron Miller for District 100"
+                    meta={[
+                        { name: 'description', content: 'Myron Miller for District 100 State Representative' },
+                        { name: 'keywords', content: 'new orleans, district 100, state representative, la, nola, louisiana' },
+                    ]}
+                >
+                </Helmet>
+
+                <Banner />
+
+                <div id="main">
+                    <section id="one" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>About Myron </h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic02})`}}>
+                            <header className="major">
+                                <h3>Platform</h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic03})`}}>
+                            <header className="major">
+                                <h3>Get Involved </h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic04})`}}>
+                            <header className="major">
+                                <h3>Media</h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic05})`}}>
+                            <header className="major">
+                                <h3>Donate</h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic06})`}}>
+                            <header className="major">
+                                <h3>Etiam</h3>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                    </section>
+                    <section id="two">
+                        <div className="inner">
+                            <header className="major">
+                                <h2>Massa libero</h2>
+                            </header>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <ul className="actions">
+                                <li><Link to="/landing" className="button next">Get Started</Link></li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+
+            </Layout>
+        )
+    }
+}
+
+export default HomeIndex
